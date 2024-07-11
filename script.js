6e330776f0461df9ab27a8cb26cf018f05d7c517
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-11 11:30:24 +0100

    add comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 console.log("Hello")
 
+// this tells it to look for the id
 const code = document.getElementById("code")
 const which = document.getElementById("which")
 const key = document.getElementById("key")
 
+// this changes it in real time
 document.addEventListener("keydown", (event) => {
     code.textContent = event.code
     which.textContent = event.which
     key.textContent = event.key
-} )
+})
